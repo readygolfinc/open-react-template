dc8a4eecdd133ae3f41cdcfa29261c73183f18de
Conner Simonsen <connersimonsen@gmail.com>
2022-03-15 22:21:24 -0600

    Initial public web iteration

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import Button from '../elements/Button';
 import { Link } from 'react-router-dom';
 import Logo from './partials/Logo';
 
@@ -106,21 +107,15 @@ const Header = ({
                     isActive && 'is-active'
                   )}>
                 <div className="header-nav-inner">
-                  <ul className={
-                    classNames(
-                      'list-reset text-xs',
-                      navPosition && `header-nav-${navPosition}`
-                    )}>
-                    <li>
-                      <Link to="#0" onClick={closeMenu}>Documentation</Link>
-                    </li>
-                  </ul>
                   {!hideSignin &&
                     <ul
                       className="list-reset header-nav-right"
                     >
                       <li>
-                        <Link to="#0" className="button button-primary button-wide-mobile button-sm" onClick={closeMenu}>Sign up</Link>
+                        {/* <Link to="#0" className="button button-primary button-wide-mobile button-sm" onClick={closeMenu}>Sign up</Link> */}
+                        <Button tag="a" color="primary" wideMobile href="https://readygolfweb.onrender.com/">
+                          Sign Up
+                        </Button>
                       </li>
                     </ul>}
                 </div>
--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -60,21 +60,32 @@ const Hero = ({
       <div className="container-sm">
         <div className={innerClasses}>
           <div className="hero-content">
-            <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+            <h1 className="mt-0 mb-16 tt-u fw-700 reveal-from-bottom" data-reveal-delay="200">
+              Ready Golf
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+                Find a tee time that works for everyone and book it instantly all in one place.
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
-                  <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
+                  {/* <Button tag="a" color="primary" wideMobile href="https://readygolfweb.onrender.com/">
                     Get started
-                    </Button>
-                  <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
-                    View on Github
-                    </Button>
+                    </Button> */}
+                <a href="https://apple.com">
+                  <Image
+                    className="has-shadow"
+                    src={require('./../../assets/images/appleAppStoreButton.png')}
+                    alt="Apple App Store Link"
+                    width={150} />
+                </a>
+                <a className='ml-8' href="https://google.com">
+                  <Image
+                    className="has-shadow"
+                    src={require('./../../assets/images/googlePlayStoreButton.png')}
+                    alt="Apple App Store Link"
+                    width={162} />
+                </a>
                 </ButtonGroup>
               </div>
             </div>
@@ -88,10 +99,9 @@ const Hero = ({
             >
               <Image
                 className="has-shadow"
-                src={require('./../../assets/images/video-placeholder.jpg')}
+                src={require('./../../assets/images/copperrock.jpeg')}
                 alt="Hero"
-                width={896}
-                height={504} />
+                width={896} />
             </a>
           </div>
           <Modal
